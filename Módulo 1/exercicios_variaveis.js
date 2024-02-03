380c6e1828ab61aaf78e8800f9f4305e255de5d6
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-02-03 19:27:09 -0300

    subindo o exercicio da aula de variaveis

---
+++ Módulo 1/exercicios_variaveis.js
@@ -0,0 +1,28 @@
+/* 
+Exercicio 1: 
+1- Criar variaveis que armazene dados de voces
+2- Exibir no console
+Melhoras: Exibir em formato String template
+*/
+
+/* 
+Exercicio 2:
+1- Criar uma variavel imutavel com seu nome
+2- Declarar cidade natal 
+3- Exibir ambos no console
+*/
+
+/* 
+Exercicio 3:
+1- Criar duas variaveis com o valor x e y
+2- Realizar 4 operações (soma, subtração, divisão, multiplicação)
+3- Exibir seu resultado
+*/
+
+/* 
+Exercicio 4:
+Pedrinho está com dificuldades  em descobrir sua nota do semestre, vamos ajuda-los?
+1- Criar Variaveis que armazene as notas da unidade 1, 2, 3
+2- Descobrir a media de Pedro
+3- Exibir a media de Pedro
+*/
